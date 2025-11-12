09c108eb52159a2d8fbb4b4530885cfb18a85f99
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2025-11-12 17:33:48 +0100

    Refactor `getNamesFromPattern` to use `acorn-walk`

--- src/getNamesFromPattern.ts
+++ src/getNamesFromPattern.ts
@@ -1,40 +1,49 @@
 import { Pattern } from "acorn";
+import walk from "acorn-walk";
 
-const getNamesFromPattern = (pattern: Pattern, names: string[]): boolean => {
-  let result = true;
-  switch (pattern.type) {
-    case "Identifier":
-      names.push(pattern.name);
-      break;
-    case "ObjectPattern":
-      for (const property of pattern.properties) {
-        switch (property.type) {
-          case "Property":
-            result &&= getNamesFromPattern(property.value, names);
-            break;
-          case "RestElement":
-            result &&= getNamesFromPattern(property.argument, names);
-            break;
-        }
+type State = { names: string[]; result: boolean };
+
+const functions: walk.RecursiveVisitors<State> = {
+  Identifier: (identifier, state) => {
+    state.names.push(identifier.name);
+  },
+  ObjectPattern: (pattern, state, callback) => {
+    for (const property of pattern.properties) {
+      switch (property.type) {
+        case "Property":
+          callback(property.value, state);
+          break;
+        case "RestElement":
+          callback(property.argument, state);
+          break;
       }
-      break;
-    case "ArrayPattern":
-      for (const element of pattern.elements) {
-        if (element === null) continue;
-        result &&= getNamesFromPattern(element, names);
-      }
-      break;
-    case "RestElement":
-      result &&= getNamesFromPattern(pattern.argument, names);
-      break;
-    case "AssignmentPattern":
-      result &&= getNamesFromPattern(pattern.left, names);
-      break;
-    default:
-      result = false;
-      break;
-  }
-  return result;
+    }
+  },
+  ArrayPattern: (pattern, state, callback) => {
+    for (const element of pattern.elements) {
+      if (element === null) continue;
+      callback(element, state);
+    }
+  },
+  RestElement: (element, state, callback) => {
+    callback(element.argument, state);
+  },
+  AssignmentPattern: (pattern, state, callback) => {
+    callback(pattern.left, state);
+  },
+  MemberExpression: (expression, state, callback) => {
+    if (expression.object.type !== "Identifier" && expression.object.type !== "MemberExpression") {
+      state.result = false;
+      return;
+    }
+    callback(expression.object, state);
+  },
+};
+
+const getNamesFromPattern = (pattern: Pattern, names: string[]) => {
+  const state: State = { names, result: true };
+  walk.recursive(pattern, state, functions);
+  return state.result;
 };
 
 export default getNamesFromPattern;
